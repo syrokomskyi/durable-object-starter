599a5601b44cd8f22e50a20788ca4e95125da975
Andrii Syrokomskyi <signmotion@gmail.com>
2024-11-24 20:59:08 +0100

    We can see `Hello world` on http://127.0.0.1:8787

--- src/index.ts
+++ src/index.ts
@@ -13,55 +13,24 @@ import { DurableObject } from "cloudflare:workers";
  * Learn more at https://developers.cloudflare.com/durable-objects
  */
 
-/** A Durable Object's behavior is defined in an exported Javascript class */
 export class MyDurableObject extends DurableObject {
-  /**
-   * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-   * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-   *
-   * @param ctx - The interface for interacting with Durable Object state
-   * @param env - The interface to reference bindings declared in wrangler.toml
-   */
-  constructor(ctx: DurableObjectState, env: Env) {
-    super(ctx, env);
-  }
-
-  /**
-   * The Durable Object exposes an RPC method sayHello which will be invoked when when a Durable
-   *  Object instance receives a request from a Worker via the same method invocation on the stub
-   *
-   * @param name - The name provided to a Durable Object instance from a Worker
-   * @returns The greeting to be sent back to the Worker
-   */
-  async sayHello(name: string): Promise<string> {
-    return `Hello, ${name}!`;
+  async sayHello(): Promise<string> {
+    return "Hello, World!";
   }
 }
 
+// Worker
 export default {
-  /**
-   * This is the standard fetch handler for a Cloudflare Worker
-   *
-   * @param request - The request submitted to the Worker from the client
-   * @param env - The interface to reference bindings declared in wrangler.toml
-   * @param ctx - The execution context of the Worker
-   * @returns The response to be sent back to the client
-   */
   async fetch(request, env, ctx): Promise<Response> {
-    // We will create a `DurableObjectId` using the pathname from the Worker request
-    // This id refers to a unique instance of our 'MyDurableObject' class above
-    const id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(
-      new URL(request.url).pathname,
-    );
+    // Every unique ID refers to an individual instance of the Durable Object class
+    const id = env.MY_DURABLE_OBJECT.idFromName("foo");
 
-    // This stub creates a communication channel with the Durable Object instance
-    // The Durable Object constructor will be invoked upon the first call for a given id
+    // A stub is a client used to invoke methods on the Durable Object
     const stub = env.MY_DURABLE_OBJECT.get(id);
 
-    // We call the `sayHello()` RPC method on the stub to invoke the method on the remote
-    // Durable Object instance
-    const greeting = await stub.sayHello("world");
+    // Methods on the Durable Object are invoked via the stub
+    const rpcResponse = await stub.sayHello();
 
-    return new Response(greeting);
+    return new Response(rpcResponse);
   },
 } satisfies ExportedHandler<Env>;
